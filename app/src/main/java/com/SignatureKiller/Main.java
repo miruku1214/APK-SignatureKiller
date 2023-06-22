c7ce925a15d39b85965e56593fd063e950022d79
miruku1214 <36main369@gmail.com>
2023-06-22 17:39:57 +0900

    From Termux

--- app/src/main/java/com/SignatureKiller/Main.java
+++ app/src/main/java/com/SignatureKiller/Main.java
@@ -8,6 +8,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.Toast;
+import android.os.Handler;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.InvocationHandler;
@@ -31,17 +33,67 @@ import java.security.NoSuchAlgorithmException;
 
 
 public class Main {
-	private static final int GET_SIGNATURES = 0x40;
+	private static Context context;
+	private static byte[][] sign;
+	private static File apkCopy;
 	
+	
+	private static Object CreatePMProxy(final Object pm) throws ClassNotFoundException {
+		Class<?> IPackageManagerC = Class.forName("android.content.pm.IPackageManager");
+		return Proxy.newProxyInstance(IPackageManagerC.getClassLoader(), new Class[] {IPackageManagerC}, new InvocationHandler() {
+			private final int GET_SIGNATURES = 0x40;
+			
+			@Override 
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				switch (method.getName()) {
+					case "getPackageInfo": {
+						String packageName = (String) args[0];
+						Integer flag = (Integer) args[1];
+						
+						if (packageName.equals(context.getPackageName())) {
+							PackageInfo info = (PackageInfo) method.invoke(pm, args);
+							if ((flag & GET_SIGNATURES) != 0) {
+								info.signatures = new Signature[sign.length];
+								for (int i = 0; i < info.signatures.length; i++) {
+									info.signatures[i] = new Signature(sign[i]);
+								}
+							}
+							info.applicationInfo.sourceDir = apkCopy.getAbsolutePath();
+							info.applicationInfo.publicSourceDir = apkCopy.getAbsolutePath();
+							return info;
+						}
+					} break;
+					case "getPackageArchiveInfo": {
+						String archivePath = (String) args[0];
+						Integer flag = (Integer) args[1];
+						
+						if (archivePath.equals(context.getApplicationInfo().sourceDir)) {
+							PackageInfo info = (PackageInfo) method.invoke(pm, args);
+							if ((flag & GET_SIGNATURES) != 0) {
+								info.signatures = new Signature[sign.length];
+								for (int i = 0; i < info.signatures.length; i++) {
+									info.signatures[i] = new Signature(sign[i]);
+								}
+							}
+							info.applicationInfo.sourceDir = apkCopy.getAbsolutePath();
+							info.applicationInfo.publicSourceDir = apkCopy.getAbsolutePath();
+							return info;
+						}
+					} break;
+				}
+				return method.invoke(pm, args);
+			}
+		});
+	}
 	
 	public static String CalcSHA256(InputStream is) throws IOException, NoSuchAlgorithmException {
 		String output;
-		int read;
-		byte[] buffer = new byte[8192];
+		byte[] buffer = new byte[1024];
+		int bufMax = 0;
 		
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		while ((read = is.read(buffer)) > 0) {
-			digest.update(buffer, 0, read);
+		while ((bufMax = is.read(buffer)) > 0) {
+			digest.update(buffer, 0, bufMax);
 		}
 		byte[] hash = digest.digest();
 		BigInteger bigInt = new BigInteger(1, hash);
@@ -57,18 +109,21 @@ public class Main {
 		final String ApkPath = "orig.apk";
 		final String SignData = "#### PASTE SIGN DATA HERE ####";
 		
+		Main.context = context;
 		try {
 			DataInputStream dis = new DataInputStream(new ByteArrayInputStream(Base64.decode(SignData, Base64.DEFAULT)));
-			final byte[][] sign = new byte[(dis.read() & 0xFF)][];
+			sign = new byte[(dis.read() & 0xFF)][];
 			for (int i = 0; i < sign.length; i++) {
 				sign[i] = new byte[dis.readInt()];
 				dis.readFully(sign[i]);
 			}
 			
 			File filesDir = context.getFilesDir();
-			filesDir.mkdirs();
+			if (!filesDir.exists()) {
+				filesDir.mkdirs();
+			}
 			
-			final File apkCopy = new File(filesDir, ApkPath);
+			apkCopy = new File(filesDir, ApkPath);
 			
 			boolean copyOrReplace = false;
 			if (!apkCopy.exists()) {
@@ -98,14 +153,17 @@ public class Main {
 				if (apkCopy.exists()) {
 					apkCopy.delete();
 				}
+				
 				InputStream apkOrigInput = context.getAssets().open(ApkPath);
 				OutputStream apkCopyOutput = new FileOutputStream(apkCopy);
-				byte[] buf = new byte[8192];
-				int read = 0;
-				while ((read = apkOrigInput.read(buf, 0, buf.length)) != -1) {
-					apkCopyOutput.write(buf, 0, read);
+				
+				byte[] buf = new byte[1024];
+				int bufMax = 0;
+				while ((bufMax = apkOrigInput.read(buf, 0, buf.length)) != -1) {
+					apkCopyOutput.write(buf, 0, bufMax);
 					apkCopyOutput.flush();
 				}
+				
 				apkOrigInput.close();
 				apkCopyOutput.close();
 			}
@@ -134,61 +192,20 @@ public class Main {
 			
 			Field sPackageManagerF = ActivityThreadC.getDeclaredField("sPackageManager");
 			sPackageManagerF.setAccessible(true);
-			final Object sPackageManager = sPackageManagerF.get(currentActivityThread);
+			Object sPackageManager = sPackageManagerF.get(currentActivityThread);
 			
-			Class<?> IPackageManagerC = Class.forName("android.content.pm.IPackageManager");
-			Object mPMProxy = Proxy.newProxyInstance(IPackageManagerC.getClassLoader(), new Class[] {IPackageManagerC}, new InvocationHandler() {
-				@Override 
-				public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-					switch (method.getName()) {
-						case "getPackageInfo": {
-							String packageName = (String) args[0];
-							Integer flag = (Integer) args[1];
-							
-							if (packageName.equals(context.getPackageName())) {
-								PackageInfo info = (PackageInfo) method.invoke(sPackageManager, args);
-								if ((flag & GET_SIGNATURES) != 0) {
-									info.signatures = new Signature[sign.length];
-									for (int i = 0; i < info.signatures.length; i++) {
-										info.signatures[i] = new Signature(sign[i]);
-									}
-								}
-								info.applicationInfo.sourceDir = apkCopy.getAbsolutePath();
-								info.applicationInfo.publicSourceDir = apkCopy.getAbsolutePath();
-								return info;
-							}
-						} break;
-						case "getPackageArchiveInfo": {
-							String archivePath = (String) args[0];
-							Integer flag = (Integer) args[1];
-							
-							if (archivePath.equals(context.getApplicationInfo().sourceDir)) {
-								PackageInfo info = (PackageInfo) method.invoke(sPackageManager, args);
-								if ((flag & GET_SIGNATURES) != 0) {
-									info.signatures = new Signature[sign.length];
-									for (int i = 0; i < info.signatures.length; i++) {
-										info.signatures[i] = new Signature(sign[i]);
-									}
-								}
-								info.applicationInfo.sourceDir = apkCopy.getAbsolutePath();
-								info.applicationInfo.publicSourceDir = apkCopy.getAbsolutePath();
-								return info;
-							}
-						} break;
-					}
-					return method.invoke(sPackageManager, args);
-				}
-			});
-			sPackageManagerF.set(currentActivityThread, mPMProxy);
+			sPackageManagerF.set(currentActivityThread, CreatePMProxy(sPackageManager));
 			
 			PackageManager pm = context.getPackageManager();
 			
 			Field mPMF = pm.getClass().getDeclaredField("mPM");
 			mPMF.setAccessible(true);
-			mPMF.set(pm, mPMProxy);
+			Object mPM = mPMF.get(pm);
 			
-			Log.i("APK-SignatureKiller Core (" + context.getPackageName() + ")", "Hook Success");
-		} catch (
+			mPMF.set(pm, CreatePMProxy(mPM));
+			
+			Log.i("APK-SignatureKiller Core (" + context.getPackageName() + ")", "Hook Success.");
+		} catch (final
 			IOException
 			| NoSuchAlgorithmException
 			| ClassNotFoundException
@@ -197,8 +214,18 @@ public class Main {
 			| InvocationTargetException
 			| IllegalAccessException ex
 		) {
-			Log.e("APK-SignatureKiller Core (" + context.getPackageName() + ")", "Hook Failed (" + ex.getMessage() + ")");
+			Log.e("APK-SignatureKiller Core (" + context.getPackageName() + ")", "Hook Failed. (" + ex.getMessage() + ")");
+			Log.e(context.getPackageName(), Log.getStackTraceString(ex));
 			ex.printStackTrace();
+			
+			Toast.makeText(context, "APK-SignatureKiller Core: Hook Failed." + "\n" + "Cause: " + ex.getClass().getName() + " (" + ex.getMessage() + ")", Toast.LENGTH_LONG).show();
+			final Handler handler = new Handler();
+			handler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					android.os.Process.killProcess(android.os.Process.myPid());
+				}
+			}, 5000);
 		}
 	}
 }
